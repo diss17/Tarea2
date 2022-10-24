b028a32adc49a3bf5ce4646b1cf49fc614354a02
Gaspar Jimenez <oficialgaspii@gmail.com>
2022-10-24 19:39:31 -0300

    Exception de NoHayBebida agregado al codigo

--- tarea2/src/tarea2/Tarea2.java
+++ tarea2/src/tarea2/Tarea2.java
@@ -308,4 +308,10 @@ class Expendedor {
     }
 
 }
+    public class NoHayBebidaException extends Exception{
+
+    public NoHayBebidaException(String errorMessage) {
+        super(errorMessage);
+    }
+}
 
