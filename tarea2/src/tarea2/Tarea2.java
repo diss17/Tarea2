88d2104ec4acdad5ecc4c3dad47d2e5a0fff8b22
Daniel Soto <danielsoto1c@gmail.com>
2022-10-24 19:17:44 -0300

    Clases Expendedor y Deposito, bugs existentes

--- tarea2/src/tarea2/Tarea2.java
+++ tarea2/src/tarea2/Tarea2.java
@@ -1,31 +1,38 @@
 package tarea2;
-public class Tarea2 {
 
+import java.util.ArrayList;
+
+public class Tarea2 {
 
     public static void main(String[] args) {
         System.out.println("main");
     }
-    
+
 }
 
 abstract class Bebida {
+
     private int num;
-    public void setSerie(int serie){
-        this.num=serie;
+
+    public void setSerie(int serie) {
+        this.num = serie;
     }
+
     public int getSerie() {
         return num;
     }
+
     public String Beber() {
         return "Bebiendo: ";
     }
 }
 
-
 class Sprite extends Bebida {
+
     public Sprite() {
         super();
     }
+
     @Override
     public String Beber() {
         return super.Beber() + "Sprite";
@@ -34,9 +41,11 @@ class Sprite extends Bebida {
 }
 
 class CocaCola extends Bebida {
+
     public CocaCola() {
         super();
     }
+
     @Override
     public String Beber() {
         return super.Beber() + "Cocacola";
@@ -44,9 +53,11 @@ class CocaCola extends Bebida {
 }
 
 class Fanta extends Bebida {
+
     public Fanta() {
         super();
     }
+
     @Override
     public String Beber() {
         return super.Beber() + "Fanta";
@@ -55,20 +66,26 @@ class Fanta extends Bebida {
 
 //Esta bien agregar el toString como lo hice en cada clase de tipo Moneda?
 abstract class Moneda {
+
     public int valor;
     public String serie;
+
     public Moneda() {
         super();
     }
-    public void setSerie(String serie){
-        this.serie=serie;
+
+    public void setSerie(String serie) {
+        this.serie = serie;
     }
+
     public String getSerie() {
         return "Moneda de " + serie;
     }
-    public void setValor(int valor){
-        this.valor=valor;
+
+    public void setValor(int valor) {
+        this.valor = valor;
     }
+
     public int getValor() {
         return valor;
     }
@@ -76,83 +93,188 @@ abstract class Moneda {
 
 //Clase moneda de 1500.
 class Moneda1500 extends Moneda {
+
     public Moneda1500() {
         super();
     }
+
     @Override
     public String getSerie() {
         return super.getSerie() + "1500";
     }
+
     @Override
     public int getValor() {
         valor = 1500;
         return super.getValor();
     }
+
     @Override
-    public String toString(){
-        return "Moneda de $1500/n°Serie: "+getSerie();
+    public String toString() {
+        return "Moneda de $1500/n°Serie: " + getSerie();
     }
 }
 
 //Clase moneda 1000.
 class Moneda1000 extends Moneda {
+
     public Moneda1000() {
         super();
     }
+
     @Override
     public String getSerie() {
         return super.getSerie() + "1000";
     }
+
     @Override
     public int getValor() {
         valor = 1000;
         return super.getValor();
     }
+
     @Override
-    public String toString(){
-        return "Moneda de $1000/n°Serie: "+getSerie();
+    public String toString() {
+        return "Moneda de $1000/n°Serie: " + getSerie();
     }
 }
 
 //Clase moneda 500.
 class Moneda500 extends Moneda {
+
     public Moneda500() {
         super();
     }
+
     @Override
     public String getSerie() {
         return super.getSerie() + "500";
     }
+
     @Override
     public int getValor() {
         valor = 500;
         return super.getValor();
     }
+
     @Override
-    public String toString(){
-        return "Moneda de $500/n°Serie: "+getSerie();
+    public String toString() {
+        return "Moneda de $500/n°Serie: " + getSerie();
     }
 }
 
 //Clase moneda 100.
 class Moneda100 extends Moneda {
+
     public Moneda100() {
         super();
     }
+
     @Override
     public String getSerie() {
         return super.getSerie() + "100";
     }
+
     @Override
     public int getValor() {
         valor = 100;
         return super.getValor();
     }
-    @Override 
-    public String toString(){
-        return "Moneda de $100/n°Serie: "+getSerie();
+
+    @Override
+    public String toString() {
+        return "Moneda de $100/n°Serie: " + getSerie();
     }
 }
 
+class Expendedor {
 
+    private int cantidad;
+    private int precio;
+    private int cualquiera;
+    private Deposito coca;
+    private Deposito sprite;
+    private Deposito fanta;
+    private Moneda Pago;
 
+    public Expendedor(int numBebidas, int precioBebidas) {
+        cantidad = numBebidas;
+        precio = precioBebidas;
+        coca = new Deposito();
+        sprite = new Deposito();
+        fanta = new Deposito();
+        for (int i = 0; i < cantidad; i++) {
+            coca.addBebida(new CocaCola(i + 100));
+            sprite.addBebida(new Sprite(i + 200));
+            fanta.addBebida(new Fanta(i + 300));
+        }
+    }
+
+    public Bebida comprarBebida(Moneda m, int cual) {
+        Pago = m;
+        //Verificar sentencia cuando se intente comprar con una moneda de tipo null
+        //(Pago incorrecto exception)
+        if (Pago.getValor() > precio) {
+            cualquiera = Pago.getValor() - precio;
+            switch (cual) {
+                case 1:
+                    return coca.getBebida();
+                case 2:
+                    return sprite.getBebida();
+                case 3:
+                    return fanta.getBebida();
+                default:
+                    return null;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    public Moneda getVuelto() {
+        if (cualquiera == 0) {
+            Pago = null;
+            return Pago;
+        } else {
+            cualquiera = cualquiera - 100;
+            return (new Moneda100());
+        }
+    }
+
+    public int vueltito() {
+        int resp = Pago.getValor() - precio;
+        return (resp);
+    }
+
+    /*    public void ValidarBebida() {
+        ControlExceptions h = new ControlExceptions();
+        if (cantidad <= 0) {
+            try {
+                h.validarBebida();
+            } catch (NoHayBebidaException b) {
+                System.out.println("Error: " + b.getMessage());
+            }
+        }
+    }
+}*/
+    class Deposito {
+
+        private ArrayList gaseosa;
+
+        public Deposito() {
+            gaseosa = new ArrayList();
+        }
+
+        public void addBebida(Bebida num) {
+            gaseosa.add(num);
+        }
+
+        public Bebida getBebida() {
+            if (gaseosa.size() <= 0) {
+                return null;
+            } else {
+                Bebida b = (Bebida) gaseosa.remove(0);
+                return b;
+            }
+        }
+    }
