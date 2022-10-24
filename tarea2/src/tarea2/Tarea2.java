5bfad675045bb4361d1260275b19c5494481bb25
Gaspar Jimenez <oficialgaspii@gmail.com>
2022-10-24 19:31:04 -0300

    Clase Comprador Agregada al code

--- tarea2/src/tarea2/Tarea2.java
+++ tarea2/src/tarea2/Tarea2.java
@@ -278,3 +278,34 @@ class Expendedor {
             }
         }
     }
+    class Comprador {
+
+    private int cantidad;
+    private String tipoBebida;
+
+    public Comprador(Moneda m, int cualBebida, Expendedor exp) {
+        Bebida aux = exp.comprarBebida(m, cualBebida);
+        ControlExceptions h = new ControlExceptions();
+        if (aux == null) {
+            cantidad = m.getValor();
+            try {
+                h.validarBebida();
+            } catch (NoHayBebidaException b) {
+                System.out.println("Error: " + b.getMessage());
+            }
+        } else {
+            tipoBebida = aux.Beber();
+            cantidad = exp.vueltito();
+        }
+
+    }
+    public int cuantoVuelto() {
+        return (cantidad);
+    }
+
+    public String queBebiste() {
+        return (tipoBebida);
+    }
+
+}
+
